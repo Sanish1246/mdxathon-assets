6e0d24de3bfd505eddd2963f5d2629c4744dd3e4
Sanish1246 <Sanishferrari@gmail.com>
2025-09-04 21:57:47 +0400

    Creating sidebar using shadcn

---
+++ shadcn-sidebar/src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Layout from '@/components/layout'
+
+
+function App() {
+
+  return (
+    <>
+         <Layout>
+      {/* Your app content goes here */}
+    </Layout>
+    </>
+  )
+}
+
+export default App
